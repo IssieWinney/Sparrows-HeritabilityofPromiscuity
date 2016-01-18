54cfa220b68761b3c0c53c49d4cd1adabf21916b
IssieWinney <isabel.winney@gmail.com>
2016-01-18 16:17:31 +0000

    # Isabel Winney # 20160118 Added code to generate ASRemlR pedigree Added code to make table usys_qbirdbroodcohortparent

--- heritabilityPromiscuity.R
+++ heritabilityPromiscuity.R
@@ -24,7 +24,7 @@ library(knitr)
 #Copyright (C) 2013 The R Foundation for Statistical Computing
 print(sessionInfo())
 
-# this is the output on 20160118
+# output pasted on 20160118
 #R version 3.0.2 (2013-09-25)
 #Platform: i386-w64-mingw32/i386 (32-bit)
 #
@@ -55,25 +55,43 @@ print(sessionInfo())
 
 source("importSparrowPedigree.R")
 
-# cut to the three first columns:
+# metadata is in importSparrowPedigree
 
+head(sparrowpedigree)
+
+# cut to the three first columns:
 sparrowped <- sparrowpedigree[,1:3]
 head(sparrowped)
 tail(sparrowped)
 
 # fix the pedigree (missing sires and dams are inserted at the top of the file)
-fixedsparrowped <- fixPedigree(sparrowped)
+{fixedsparrowped <- fixPedigree(sparrowped)
 
 head(fixedsparrowped)
 tail(fixedsparrowped)
 
 summary(fixedsparrowped)
-str(fixedsparrowped)
+str(fixedsparrowped)}
 
-# in previous work with the pedigree, I have imputed missing dams.
+
+# in previous work with the pedigree, I have imputed dams.
 # This involves taking every individual with no assigned dam and
 # assigning an unique dam to each of those individuals.
 # at the moment, I will not be doing this in this script.
+
+# The same can be done with the ASReml-R ready version of the pedigree:
+{asrped <- sparrowpedigree[,5:7]
+head(asrped)
+tail(asrped)
+
+# fix the pedigree (missing sires and dams are inserted at the top of the file)
+fixedasrped <- fixPedigree(asrped)
+
+head(fixedasrped)
+tail(fixedasrped)
+
+summary(fixedasrped)
+str(fixedasrped)}
 
 ##############################################################################
 # loading the Lundy House Sparrow Database
@@ -89,6 +107,21 @@ sqlTables(sparrowDB)
 # offspring data
 ##############################################################################
 
+# Assumptions:
+# 1   males are all dead --> the calculated phenotypes
+#     are complete. This is not true but can be reconsidered later
+#     later.
+# 2   the 2013 pedigree is ok.
+# 3   a brood has just one genetic mother. By looking
+#     up the genetic mother of a brood, the mother that is
+#     returned first is the one used in the analysis below. 
+# 4   I can only make phenotypes using individuals that have been born
+#     in a brood, otherwise I do not know their social and genetic parents
+#     i.e. for a mother, I can only calculate her EPO and WPO from EPO/WPO
+#     born in broods
+# 5   All cohorts in the database are accurate (not true)
+
+
 # I have made a list using the database from March 2015
 # database called DataBaseV0.74-updated20150320-AlfredoCheckedMarch2015
 # of all the birds associated with a brood, their cohorts,
@@ -98,8 +131,27 @@ sqlTables(sparrowDB)
 # based on these offspring and all the offspring that are
 # caught in the winter etc I ignore.
 
-# This is the Git version of an existing script
+# This is the Git version of an existing script in which I explored
+# how to make the data and use the sparrow data set for an analysis
+# of this.
 
+usys_qbirdbroodcohortparent <- sqlQuery (sparrowDB, 
+                                        "SELECT tblBirdID.BirdID, tblBirdID.Cohort, tblBirdID.BroodRef, tblBroods.BroodName, tblBroods.NestboxRef, tblBroods.SocialDadID, tblBroods.SocialDadCertain, tblBroods.SocialMumID, tblBroods.SocialMumCertain
+                                        FROM tblBroods 
+                                        INNER JOIN tblBirdID 
+                                        ON tblBroods.BroodRef = tblBirdID.BroodRef;")
+
+head(usys_qbirdbroodcohortparent)
+tail(usys_qbirdbroodcohortparent)
+str(usys_qbirdbroodcohortparent)
+
+##############################################################################
+# Additional consideration 1: exclude birds that are still alive
+##############################################################################
+
+# The consideration is that birds that are still alive are still 
+# having EPO and WPO, so our phenotype is not completely accurate.
+# What happens to the analysis without these birds?
 
 ##############################################################################
 # Close the database
