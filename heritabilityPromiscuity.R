795fb6e1a58b45bb8a111656d5c3853c36b5f521
IssieWinney <isabel.winney@gmail.com>
2016-01-19 17:13:01 +0000

    Adding analysis and construction of data frames for males (lifetime) and males (per year).

--- heritabilityPromiscuity.R
+++ heritabilityPromiscuity.R
@@ -65,13 +65,19 @@ head(sparrowped)
 tail(sparrowped)
 
 # fix the pedigree (missing sires and dams are inserted at the top of the file)
-{fixedsparrowped <- fixPedigree(sparrowped)
+{
+  fixedsparrowped <- fixPedigree(sparrowped)
 
-head(fixedsparrowped)
-tail(fixedsparrowped)
+  head(fixedsparrowped)
+  tail(fixedsparrowped)
 
-summary(fixedsparrowped)
-str(fixedsparrowped)}
+  summary(fixedsparrowped)
+  str(fixedsparrowped)
+  
+  # re-name the pedigree to be animal, dam, sire
+  names(fixedsparrowped) <- c("animal", "dam", "sire")
+  head(fixedsparrowped)
+}
 
 
 # in previous work with the pedigree, I have imputed dams.
@@ -93,6 +99,10 @@ str(fixedsparrowped)}
 
   summary(fixedasrped)
   str(fixedasrped)
+  
+  # re-name the pedigree to be animal, dam, sire
+  names(fixedasrped) <- c("animal", "dam", "sire")
+  head(fixedasrped)
 }
 
 ##############################################################################
@@ -240,7 +250,31 @@ table(offspring3$EPO, offspring3$WPO)
 }
 
 ##############################################################################
-# male phenotypes
+# cohort data
+##############################################################################
+
+# extract the list of cohorts and Bird IDs
+
+{
+  sqlFetch(sparrowDB, "tblBirdID", max=10)
+
+  birdcohort <- sqlQuery (sparrowDB,
+                       "SELECT tblBirdID.BirdID, 
+                       tblBirdID.Cohort
+                       FROM tblBirdID;",
+                      na.strings="NA")
+  
+  head(birdcohort)
+  summary(birdcohort)
+  # a missing cohort???
+  birdcohort[which(is.na(birdcohort$Cohort)),]
+  # this is a blank record from my last year :s
+  # rest of the data seems fine
+}
+
+
+##############################################################################
+# male phenotypes 
 ##############################################################################
 
 # Now aggregate the data in to a phenotype for each male.
@@ -248,6 +282,10 @@ table(offspring3$EPO, offspring3$WPO)
 # the number of EPO and WPO that each father sires.
 # This means that males that sire no offspring are NOT in 
 # this data set
+
+#-------------------------------------------
+# Male dataset of genetic offspring per lifetime
+#-------------------------------------------
 
 {
   # make a list of unique males and count the number of WPO per male
@@ -280,7 +318,7 @@ table(offspring3$EPO, offspring3$WPO)
     
   # and that no males are different
   malephenotypes$animal!=malephenotypes$Group.1
-  }
+  
     
   # good!
   
@@ -290,14 +328,352 @@ table(offspring3$EPO, offspring3$WPO)
     
   
   # are any males in my file not in the pedigree?
-  check1 <- fixedsparrowped$id[match(malephenotypes$animal,
-                                  fixedsparrowped$id)]
+  check1 <- fixedsparrowped$animal[match(malephenotypes$animal,
+                                  fixedsparrowped$animal)]
   
   summary(check1)
   check1
   # wooo! So, no NA's means that all males are in the pedigree.
 }
 
+{
+  # add maternal and paternal ID of each male:
+  
+  # convert factors to numeric:
+  tail(fixedsparrowped)
+  fixedsparrowped$animal <- as.numeric(levels(fixedsparrowped$animal))[fixedsparrowped$animal]
+  fixedsparrowped$dam <- as.numeric(levels(fixedsparrowped$dam))[fixedsparrowped$dam]
+  fixedsparrowped$sire <- as.numeric(levels(fixedsparrowped$sire))[fixedsparrowped$sire]
+  tail(fixedsparrowped)
+  
+  # maternal ID
+  head(malephenotypes)
+  malephenotypes$maternalID <- fixedsparrowped$dam[match(malephenotypes$animal,
+                                                         fixedsparrowped$animal)]
+  
+  # and paternal ID
+  malephenotypes$paternalID <- fixedsparrowped$sire[match(malephenotypes$animal,
+                                                         fixedsparrowped$animal)]
+  
+  head(malephenotypes)
+  
+  # do a check
+  fixedsparrowped[which(fixedsparrowped$animal==500),]
+  malephenotypes[which(malephenotypes$animal==500),]
+  
+  # that is correct!
+  
+  fixedsparrowped[which(fixedsparrowped$animal==4722),]
+  malephenotypes[which(malephenotypes$animal==4722),]
+
+  # also correct! Looking good!
+  
+  str(malephenotypes)
+}
+
+{
+  # Add the cohort of each male:
+  malephenotypes$Cohort <- birdcohort$Cohort[match(malephenotypes$animal,
+                                                   birdcohort$BirdID)]
+  
+  head(malephenotypes)
+  summary(malephenotypes)
+  
+  # check:
+  sparrowpedigree[which(sparrowpedigree$birdid==4722),]
+  malephenotypes[which(malephenotypes$animal==4722),]
+  # good good.
+}
+
+{
+  # set appropriate factors for analysis
+  str(malephenotypes)
+  malephenotypes$factoranimal <- as.factor(malephenotypes$animal)
+  malephenotypes$factormaternalID <- as.factor(malephenotypes$maternalID)
+  malephenotypes$factorpaternalID <- as.factor(malephenotypes$paternalID)
+  malephenotypes$factorcohort <- as.factor(malephenotypes$Cohort)
+  str(malephenotypes)
+}
+
+#-------------------------------------------
+# Male dataset of genetic offspring per year
+#-------------------------------------------
+
+{
+  # per year per GENETIC male identifier:
+  offspring3$maleyear <- paste(offspring3$GeneticDadID, offspring3$Cohort, sep=".")
+  
+  head(offspring3)
+  table(offspring3$maleyear)
+  table(table(offspring3$maleyear))
+  # up to 25 offspring!
+  
+  
+  # aggregate number of WPO per male
+  maleyearWPO <- aggregate(offspring3$WPO, 
+                           list(offspring3$maleyear),
+                           FUN=sum)
+  head(maleyearWPO)
+  
+  # aggregate number of EPO per male
+  maleyearEPO <- aggregate(offspring3$EPO, 
+                           list(offspring3$maleyear),
+                           FUN=sum)
+  head(maleyearEPO)
+  
+  
+  names(maleyearWPO) <- c("animal", "WPO")
+  head(maleyearWPO)
+  
+  str(maleyearWPO)
+  str(maleyearEPO)
+  
+  
+  maleyear <- cbind(maleyearWPO, maleyearEPO)
+  
+  head(maleyear)
+  
+  # check that all data rows are matched to the right male:
+  which(maleyear$animal==maleyear$Group.1)
+  # no missing numbers --> good
+  which(maleyear$animal!=maleyear$Group.1)
+  # no missmatch --> correct
+  
+  # rename data set
+  names(maleyear) <- c("maleyear1", "WPO", "maleyear", "EPO")
+  head(maleyear)
+  
+  
+  # add the male's ID:
+  maleyear$animal <- offspring3$GeneticDadID[match(maleyear$maleyear, offspring3$maleyear)]
+  head(maleyear)
+  
+  # add male ID for repeated measures:
+  maleyear$maleid <- maleyear$animal
+  
+}
+  
+{
+  # add maternal ID for maternal effects:
+  maleyear$maternalID <- fixedsparrowped$dam[match(maleyear$animal,
+                                              fixedsparrowped$animal)]
+  head(maleyear)
+  
+  # and paternal ID
+  maleyear$paternalID <- fixedsparrowped$sire[match(maleyear$animal,
+                                                        fixedsparrowped$animal)]
+  
+  summary(maleyear)
+}
+
+{
+  # Add the cohort of each male:
+  maleyear$Cohort <- birdcohort$Cohort[match(maleyear$animal,
+                                                   birdcohort$BirdID)]
+  
+  head(maleyear)
+  summary(maleyear)
+  
+  # check:
+  sparrowpedigree[which(sparrowpedigree$birdid==4722),]
+  maleyear[which(maleyear$animal==4722),]
+  # good good.
+}
+
+{
+  # set appropriate factors for analysis
+  str(maleyear)
+  maleyear$factoranimal <- as.factor(maleyear$animal)
+  maleyear$factormaternalID <- as.factor(maleyear$maternalID)
+  maleyear$factorpaternalID <- as.factor(maleyear$paternalID)
+  maleyear$factorcohort <- as.factor(maleyear$Cohort)
+  str(maleyear)
+}
+
+##############################################################################
+# Pruning the pedigree
+##############################################################################
+
+# each pedigree can be pruned to speed up the fitting of models.
+# Pedigrees are pruned to only contain phenotyped individuals and their
+# relatives. Uninformative individuals are removed.
+{
+  # prunePed requires a numeric bird ID so check that the fixed pedigree is numeric:
+  # (https://stat.ethz.ch/pipermail/r-sig-mixed-models/2014q1/021604.html)
+  
+  str(fixedsparrowped)
+  
+  # now prune the pedigree
+  prunedped.malelifetime <- prunePed(pedigree=fixedsparrowped, 
+                                     keep=malephenotypes$animal)
+  
+  head(prunedped.malelifetime)
+  
+  # take a look at the pruned info:
+  drawPedigree(prunedped.malelifetime)
+  pedigreeinformation <- pedigreeStats(prunedped.malelifetime)
+  pedStatSummary(pedigreeinformation)
+  
+  
+}
+
+# and then the pedigrees can be converted to relationship matrices
+# for the current version of MCMCglmm:
+{
+  invped.malelifetime <- inverseA(prunedped.malelifetime)$Ainv
+  summary(invped.malelifetime)
+}
+
+##############################################################################
+# Plots of variables
+##############################################################################
+
+# how many EPO offspring do males have?
+hist(malephenotypes$EPO, breaks=seq(-1,26,1))
+# lots of zeroes.
+
+# how many are EPO?
+hist(malephenotypes$EPO/(malephenotypes$EPO+malephenotypes$WPO))
+# lots of males with no EPO, some males with only EPO
+
+# in the per year data?
+hist(maleyear$EPO, breaks=seq(-1,16,1))
+hist(maleyear$EPO/(maleyear$EPO+maleyear$WPO))
+
+##############################################################################
+# Priors
+##############################################################################
+
+# parameter expanded priors. Number before G is number of random effects. 
+# .p for parameter expansion
+{
+  prior3G.p = list(R=list(V=1,nu=0.002),
+                  G=list(G1=list(V = 1, nu = 1, alpha.mu = 0, alpha.V = 1000),
+                         G2=list(V = 1, nu = 1, alpha.mu = 0, alpha.V = 1000),
+                         G3=list(V = 1, nu = 1, alpha.mu = 0, alpha.V = 1000)))
+  
+  prior4G.p = list(R=list(V=1,nu=0.002),
+                   G=list(G1=list(V = 1, nu = 1, alpha.mu = 0, alpha.V = 1000),
+                          G2=list(V = 1, nu = 1, alpha.mu = 0, alpha.V = 1000),
+                          G3=list(V = 1, nu = 1, alpha.mu = 0, alpha.V = 1000),
+                          G4=list(V = 1, nu = 1, alpha.mu = 0, alpha.V = 1000)))
+}
+
+##############################################################################
+# Repeatability
+##############################################################################
+
+{
+  maler2EPO <- MCMCglmm(cbind(EPO, WPO)~1,
+                      ~factormale,
+                      prior=prior1G,
+                      data=maleyear,
+                      family="multinomial2",
+                      nitt=1000000,
+                      thin=800,
+                      burnin=200000)
+
+  plot(maler2EPO)
+}
+
+##############################################################################
+# Heritability
+##############################################################################
+
+# a model of heritability should have as a minimum the ID of the bird (if there
+# are repeated measures), a pedigree linked bird ID, and a maternal ID.
+# Strongly recommended: cohort
+
+#-------------------------------------------
+# Lifetime male behaviour as a multinomial variable
+#-------------------------------------------
+
+{
+  maleh2EPO.multinomial.lifetime <- MCMCglmm(cbind(EPO, WPO)~1,
+                               ~factoranimal + factormaternalID + factorcohort,
+                               ginverse=list(factoranimal=invped.malelifetime),
+                               prior=prior3G.p,
+                               data=malephenotypes,
+                               family="multinomial2",
+                               nitt=1000000,
+                               thin=800,
+                               burnin=200000)
+  
+  plot(maleh2EPO.multinomial.lifetime)
+  # strong maternal ID effect, little animal and cohort effects
+  autocorr(maleh2EPO.multinomial.lifetime$Sol)
+  autocorr(maleh2EPO.multinomial.lifetime$VCV)
+  # animal:animal is a little high - this needs to be run for longer
+}
+
+# additional consideration: whether incidental assortative mating by promiscuity
+# leads to an artificially strong maternal effect. In this case, we would expect
+# a paternal effect of comparable magnitude to the maternal effect.
+
+{
+  maleh2EPO.multinomial.lifetime.patID <- MCMCglmm(cbind(EPO, WPO)~1,
+                                             ~factoranimal + factormaternalID + 
+                                               factorpaternalID + factorcohort,
+                                             ginverse=list(factoranimal=invped.malelifetime),
+                                             prior=prior4G.p,
+                                             data=malephenotypes,
+                                             family="multinomial2",
+                                             nitt=1000000,
+                                             thin=800,
+                                             burnin=200000)
+  
+  plot(maleh2EPO.multinomial.lifetime.patID)
+  # with the addition of paternal ID, the maternal effect is much harder to 
+  # estimate and the paternal effect is of comparable size, so maybe it is
+  # an assortative mating thing. Cohort effects are clearer now.
+}
+
+#-------------------------------------------
+# Lifetime male behaviour as a poisson variable
+#-------------------------------------------
+
+{
+  maleh2EPO.poisson.lifetime <- MCMCglmm(EPO~1,
+                                             ~factoranimal + factormaternalID + factorcohort,
+                                             ginverse=list(factoranimal=invped.malelifetime),
+                                             prior=prior3G.p,
+                                             data=malephenotypes,
+                                             family="poisson",
+                                             nitt=1000000,
+                                             thin=800,
+                                             burnin=200000)
+  
+  plot(maleh2EPO.poisson.lifetime)
+  # much more consistently estimated maternal ID and cohort in this
+  # compared to the multinomial model
+  autocorr(maleh2EPO.poisson.lifetime$Sol)
+  autocorr(maleh2EPO.poisson.lifetime$VCV)
+  # all fine enough.
+}
+
+# with paternal ID included:
+
+{
+  maleh2EPO.poisson.lifetime.patID <- MCMCglmm(EPO~1,
+                                         ~factoranimal + factormaternalID +
+                                           factorpaternalID + factorcohort,
+                                         ginverse=list(factoranimal=invped.malelifetime),
+                                         prior=prior4G.p,
+                                         data=malephenotypes,
+                                         family="poisson",
+                                         nitt=1000000,
+                                         thin=800,
+                                         burnin=200000)
+  
+  plot(maleh2EPO.poisson.lifetime.patID)
+  # now, no paternal variance estimated. Maternal and cohort stay strong.
+  autocorr(maleh2EPO.poisson.lifetime.patID$Sol)
+  autocorr(maleh2EPO.poisson.lifetime.patID$VCV)
+  # again, good sampling.
+}
+
+# problems with these models: males that are older can contribute more
+# EPO. Male lifespan would need to go in a lifetime model.
 
 ##############################################################################
 # Additional consideration 1: exclude birds that are still alive
